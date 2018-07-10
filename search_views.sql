581cc6a585d5d48bf3e8e4091035fe0155c003c4
tHadd33 <37974901+tHadd33@users.noreply.github.com>
2018-07-10 14:44:52 +0100

    Amend SEARCH_VIEWS
    Added a comment and arranged the SELECT statement

--- search_views.sql
+++ search_views.sql
@@ -1,5 +1,8 @@
+--Search views
 SELECT *
-FROM (SELECT view_name, dbms_metadata.get_ddl( 'VIEW', view_name, 'OPTDEV' ) ddl, rownum
+FROM (SELECT view_name
+      , dbms_metadata.get_ddl( 'VIEW', view_name, 'OPTDEV' ) ddl
+      , rownum
         FROM user_views
 )
 WHERE ddl like '%<search text>%'
